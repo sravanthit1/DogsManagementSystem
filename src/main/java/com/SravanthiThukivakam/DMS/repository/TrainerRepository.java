b9525f05166ef5f085b037c8fdaa849ccd3c0168
Sravanthi Thukivakam <S559284@nwmissouri.edu>
2023-11-12 03:57:17 -0600

    dogs management system

---
+++ src/main/java/com/SravanthiThukivakam/DMS/repository/TrainerRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.SravanthiThukivakam.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.SravanthiThukivakam.DMS.Models.Trainer;
+
+/**
+ *@author Sravanthi Thukivakam
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
